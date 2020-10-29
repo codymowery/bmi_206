98761aaa7c7cb6450759b200ed0e3775e0637d0e
Cody Mowery <mowery.cody@gmail.com>
2020-10-29 11:38:33 -0700

    lab 3 save

--- lab3/enrichment_CMowery.R
+++ lab3/enrichment_CMowery.R
@@ -1,7 +1,7 @@
 #load packages
 library(regioneR)
 set.seed(10)
-
+setwd('~/github/bmi_206/lab3/')
 #read in BED formatted region files: all tested regions and two sets of positives
 # these are in hg19 human genome assembly coordinates
 all=toGRanges(read.table("all.bed",sep="\t"))
@@ -9,25 +9,50 @@ hits1=toGRanges(read.table("hits1.bed",sep="\t"))
 hits2=toGRanges(read.table("hits2.bed",sep="\t"))
 
 #Q1. How many regions are in hits1? How many in hits2? 
-
+length(hits1) ## 140
+length(hits2) ## 322
 
 #Q2. Are hits1 and hits2 proper subsets of all the tested regions?
 #    Check how many of each set overlaps a region in all.
-
+hits1xall = overlapRegions(hits1, all)
+overlapGraphicalSummary(hits1, all, regions.labels=c('hits1', 'all'))
+dim(hits1xall)
+table(hits1xall$type)
+## Looks like every hits1 region is in all (equal=140), but there's an additional region in all that's inside a hits1 region (BinA)
+hits2xall = overlapRegions(hits2, all)
+overlapGraphicalSummary(hits2, all, regions.labels=c('hits2', 'all'))
+dim(hits2xall)
+table(hits2xall$type)
+## Similarly, every hits2 region is in all (equal=322) 
+## but there are additional all regions in hits2 regions (i.e. AinB, + vice versa) and partially overlapping regions (i.e. ArightB)
 
 #The next few questions explore the overlap of genomic regions in hits1 and hits2.
-
-#Q3. How many regions overlap?
+hitsxhits = suppressWarnings(overlapRegions(hits1, hits2))
+overlapGraphicalSummary(hits1, hits2, regions.labels=c('hits1', 'hits2'))
+#Q3. How many regions overlap? 
+## 6 regions overlap
 #    How many regions are exactly identical? 
+## All 6 overlapping regions are exactly identical
 
 
 #Q4. Generate a set of random genomic regions of the same size as hits1.
-#     Match these to the mean and sd of the genomic length of the hits1 regions. 
-#    Do the random genomic regions overlap hits2 more or less than hits1 does? 
+#     Match these to the mean and sd of the genomic length of the hits1 regions.
+random = createRandomRegions(nregions = length(hits1), 
+                             length.mean = mean(width(hits1)),
+                             length.sd = sd(width(hits1)),
+                             genome = 'hg19')
+#    Do the random genomic regions overlap hits2 more or less than hits1 does?
+overlapRegions(random, hits2)
+## None of the randomly generated regions overlap with hits2
 #    How much do the random genomic regions overlap all tested regions?
+overlapRegions(random, all) 
+## None of the randomly generated regions overlap with all
 #    Repeatedly generate genomic regions to compute a z-score for the overlap of hits1 with hits2
 #     Use the set of overlaps with random regions to test the null hypothesis
 #     that hits2 overlaps hits1 more than expected compared to totally random regions
+randomize = function(x){createRandomRegions(genome = all)}
+pt1 = suppressWarnings(permTest(A=hits1, B=hits2, ntimes=100, randomize.function = randomize, evaluate.function = numOverlaps, genome = all,
+                                force.parallel = FALSE))
 #    What is the smallest p-value you could have gotten? 
 #    How do the results change with number of resamples? Random seed?
 
@@ -64,21 +89,3 @@ hits2=toGRanges(read.table("hits2.bed",sep="\t"))
 #          On what variables will you match them?
 #            e.g., spacing, chromosome, GC-content, distance to nearest gene, masking
 #          How does matching affect the z-score and p-value?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
