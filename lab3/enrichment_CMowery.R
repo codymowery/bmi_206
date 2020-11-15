265d9371a8b9daedb80935d26ab63f9ad7bd284c
Cody Mowery <mowery.cody@gmail.com>
2020-11-15 14:49:56 -0800

    group project code

--- lab3/enrichment_CMowery.R
+++ lab3/enrichment_CMowery.R
@@ -1,5 +1,6 @@
 #load packages
 library(regioneR)
+library(BSgenome.Hsapiens.UCSC.hg19)
 set.seed(10)
 setwd('~/github/bmi_206/lab3/')
 #read in BED formatted region files: all tested regions and two sets of positives
@@ -14,6 +15,7 @@ length(hits2) ## 322
 
 #Q2. Are hits1 and hits2 proper subsets of all the tested regions?
 #    Check how many of each set overlaps a region in all.
+## Here I am using the overlapRegions() function to look for shared and/or overlapping regions in the two region sets, hits1 and all
 hits1xall = overlapRegions(hits1, all)
 overlapGraphicalSummary(hits1, all, regions.labels=c('hits1', 'all'))
 dim(hits1xall)
@@ -27,7 +29,7 @@ table(hits2xall$type)
 ## but there are additional all regions in hits2 regions (i.e. AinB, + vice versa) and partially overlapping regions (i.e. ArightB)
 
 #The next few questions explore the overlap of genomic regions in hits1 and hits2.
-hitsxhits = suppressWarnings(overlapRegions(hits1, hits2))
+hitsxhits = overlapRegions(hits1, hits2)
 overlapGraphicalSummary(hits1, hits2, regions.labels=c('hits1', 'hits2'))
 #Q3. How many regions overlap? 
 ## 6 regions overlap
@@ -37,48 +39,172 @@ overlapGraphicalSummary(hits1, hits2, regions.labels=c('hits1', 'hits2'))
 
 #Q4. Generate a set of random genomic regions of the same size as hits1.
 #     Match these to the mean and sd of the genomic length of the hits1 regions.
+## Here I am using the createRandomRegions() function to pull random regions from the hg19 genome
+## As coded, these random region set will be the same size as hits1 with the same (or similar) mean and SD
+## Pulling random regions from hg19 with characteristics similar to hits1 will test whether hits1 x hits2 overlap is simply random and expected by chance
 random = createRandomRegions(nregions = length(hits1), 
                              length.mean = mean(width(hits1)),
                              length.sd = sd(width(hits1)),
                              genome = 'hg19')
+
 #    Do the random genomic regions overlap hits2 more or less than hits1 does?
 overlapRegions(random, hits2)
-## None of the randomly generated regions overlap with hits2
+## None of the randomly generated regions overlap with hits2, so less than with hits1
+
 #    How much do the random genomic regions overlap all tested regions?
 overlapRegions(random, all) 
-## None of the randomly generated regions overlap with all
+## None of the randomly generated regions overlap with all, so less than with hits1
+
+## My thought is that randomly selecting regions throughout the genome is a sufficiently large search space that we don't get any overlap
+## But we know that hits1 and hits2 are subsets of all, so it makes more sense to randomly sample from all and test that random subset against hits2 overlap
+
+
 #    Repeatedly generate genomic regions to compute a z-score for the overlap of hits1 with hits2
 #     Use the set of overlaps with random regions to test the null hypothesis
 #     that hits2 overlaps hits1 more than expected compared to totally random regions
-randomize = function(x){createRandomRegions(genome = all)}
-pt1 = suppressWarnings(permTest(A=hits1, B=hits2, ntimes=100, randomize.function = randomize, evaluate.function = numOverlaps, genome = all,
-                                force.parallel = FALSE))
+overlapPermTest(A=hits1, B=hits2, ntimes=100)
+#### OUTPUTS ####
+# P-value: 0.0099009900990099
+# Z-score: 24.8865
+# Alternative: greater
+# Number of iterations: 100
+# Alternative: greater
+# Evaluation of the original region set: 6
+# Evaluation function: numOverlaps
+# Randomization function: randomizeRegions
+overlapPermTest(A=random, B=hits2, ntimes=100)
+# P-value: 0.0693069306930693
+# Z-score: 3.9383
+# Number of iterations: 100
+# Alternative: greater
+# Evaluation of the original region set: 1
+# Evaluation function: numOverlaps
+# Randomization function: randomizeRegions
+
+## Based on the larger z-score for the first perm test, it appears that the hits1 x hits2 overlap is more frequent than is expected from a random hg19 sample
+## Further, there is insignificant (p=0.0693069306930693) overlap between random and hits2
+
 #    What is the smallest p-value you could have gotten? 
 #    How do the results change with number of resamples? Random seed?
+overlapPermTest(A=hits1, B=hits2, ntimes=500)
+# P-value: 0.00199600798403194
+# Z-score: 21.335
+overlapPermTest(A=hits1, B=hits2, ntimes=1000)
+# P-value: 0.000999000999000999
+# Z-score: 21.0876
+set.seed(30)
+overlapPermTest(A=hits1, B=hits2, ntimes=1000)
+# P-value: 0.000999000999000999
+# Z-score: 20.224
+
+## The z-score appears relatively stable, but the p-value expectedly gets smaller with a larger number of permutations
+
+set.seed(10) ## setting seed back to 10 as beginning
 
 
 #Q5. Repeat Q4 switching the roles of hits1 and hits2. Are conclusions similar? 
+#     Match these to the mean and sd of the genomic length of the hits2 regions.
+random = createRandomRegions(nregions = length(hits2), 
+                             length.mean = mean(width(hits2)),
+                             length.sd = sd(width(hits2)),
+                             genome = 'hg19')
+
+#    Do the random genomic regions overlap hits2 more or less than hits1 does?
+overlapRegions(random, hits1)
+## None of the randomly generated regions overlap with hits1
+
+#    How much do the random genomic regions overlap all tested regions?
+overlapRegions(random, all) 
+## None of the randomly generated regions overlap with all
+
+overlapPermTest(A=random, B=hits1, ntimes=100)
+# P-value: 0.910891089108911
+# Z-score: -0.3129
+# Number of iterations: 100
+# Alternative: less
+# Evaluation of the original region set: 0
+# Evaluation function: numOverlaps
+# Randomization function: randomizeRegions
+
+## This random sample with hits2 characteristics is even less significantly overlapping with hits1 than was the previous random sample
+## It's z-score is closer to zero and the P-value is very close to 1
+
 
 
 #Q6. Create a random bootstrap sample of regions from all tested regions. 
+random = resampleRegions(A = hits1, universe = all)
+length(random) ## 140, matches hits1
 #    Do these random regions overlap hits2 more or less than hits1 does?
+randomxhits2 = overlapRegions(random, hits2)
+overlapGraphicalSummary(random, hits2, regions.labels=c('random','hits2'))
+dim(randomxhits2)
+table(randomxhits2$type)
+## This random sample of all overlaps with hits2 more frequently than does hits1
+## There are 28 regions that are exactly the same, and 1 region with partial overlap (AleftB)
+permTest(A=hits1, ntimes=100, randomize.function=resampleRegions, universe=all,
+         evaluate.function=numOverlaps, B=hits2, verbose=FALSE)
+# P-value: 0.0099009900990099
+# Z-score: -4.4091
+# Number of iterations: 100
+# Alternative: less
+# Evaluation of the original region set: 6
+# Evaluation function: numOverlaps
+# Randomization function: resampleRegions
+
 #    How does this test differe from the one in Q4? Look at the z-score and p-value.
+## It's a relatively close-to-zero and negative Z-score, though the P-value is still significant 
+
+## When I run a permutation test with a random sample of all matching hits1 characteristics and tested against hits2,
+##  we get a negative z-score and significant p-value
 
 
 #Q7. Repeat Q6 switching the role of hits1 and hits2. Are conclusions similar?
+random = resampleRegions(A = hits2, universe = all)
+length(random) ## 322, matches hits2
+#    Do these random regions overlap hits2 more or less than hits1 does?
+randomxhits1 = overlapRegions(random, hits1)
+overlapGraphicalSummary(random, hits1, regions.labels=c('random','hits1'))
+dim(randomxhits2)
+table(randomxhits2$type)
+## This random sample of all overlaps with hits1 more frequently than does hits2
+## There are 20 regions that are exactly the same ('equal')
+permTest(A=hits2, ntimes=100, randomize.function=resampleRegions, universe=all,
+         evaluate.function=numOverlaps, B=hits1, verbose=FALSE)
+# P-value: 0.0099009900990099
+# Z-score: -4.1402
+# Number of iterations: 100
+# Alternative: less
+# Evaluation of the original region set: 6
+# Evaluation function: numOverlaps
+# Randomization function: resampleRegions
 
+## Again, the p-value is significant but the z-score is negative and closer to zero
 
 #Q8. Which null distribution would you use in your own research and why?  
+## I think the random sample of "all" is a better null distribution than is the random sample of "hg19"
+## because the first questions above reveal that hits1 and hits2 are subsets of 'all', 
+##  it makes sense to compare their overlap to that of a random sample of 'all' with the same characteristics (size, mean, sd) as one of the groups
+## This allows us to more confidently conclude whether or not to reject the null hypothesis that hits1 and hits2 overlap the same amount as expected by chance
 
 
 #The next few questions involve downloading genomics data
 # You can choose sets of regions, e.g, gene annotation, ChIPseq, RNAseq, ATACseq, GWAS SNPs
+cpg <- toGRanges("http://www.haowulab.org/software/makeCGI/model-based-cpg-islands-hg19.txt")
 
 #Q9. Using data you download, can you infer what function was tested in the assay that discovered
 #     hits1 and hits2? Choose data sets that will be informative about candidate functions.
 #     Compute overlaps or mean values of the downloaded data for the union of hits1 and hits2
 #     Guess what type of genomic element these hits are (i.e., what assay was performed)
-
+cpgxhits1 = overlapRegions(cpg, hits1)
+table(cpgxhits1$type)
+cpgxhits2 = overlapRegions(cpg, hits2)
+table(cpgxhits2$type)
+cpgxall = overlapRegions(cpg, all)
+table(cpgxall$type)
+## There is quite a bit of overlap between this cpg islands dataset (pulled from regioneR documentation) and the all, hits1, hits2 datasets
+## Because CpG islands are enriched in gene promoters, and there is a bit of overlap as mentioned above, 
+## and this overlap is predominantly "AinB" meaning the CpG region(s) are within the larger all/hits1/hits2 regions (like CpG islands are enriched within promoters)
+##    I believe that "all" is a subset of predicted or experimentally measured gene promoters
 
 #BONUS Q10. Do you think hits1 and hits2 function in the same cell type? 
 #     Build on your analysis in Q9 by separately testing overlaps with hits1 and hits2
--- project/atac_code.R
+++ project/atac_code.R
@@ -9,7 +9,6 @@ length(grep('AMD', grep('Retina', colnames(infile), value = TRUE)))
 
 long = pivot_longer(infile, cols = grep('Retina', colnames(infile), value = TRUE), names_to = 'Sample', values_to = 'counts')
 long
-save_rds(long, 'GSE99287_Retina_ATACSeq_peak_counts.txt')
 table(is.na(long$counts))
 
 table(is.na(infile[,9:ncol]))
@@ -21,9 +20,14 @@ table(long$Sample)
 
 rho = long %>% filter(Gene == 'RHO')
 
+
+## Load in normalized count table
+infile = read.csv('~/github/bmi_206/project/GSE99287_normed_counts.csv')
 ## Fig 2D
 library(ggpubr)
 library(ggExtra)
+library(tidyverse)
+library(gridExtra)
 fig2d = infile %>% 
   mutate(fc = AMD2_Retina_MacL/AMD2_Retina_MacR,
          avg = rowMeans(select(., grep('AMD2_Retina_Mac', colnames(infile))), na.rm = TRUE))
@@ -37,15 +41,32 @@ p = ggplot(fig2d, aes(log2(avg), log2(fc)))+
   ylab('Log2(FC) of Left vs Right Eye in  AMD2')+
   scale_x_continuous(limits = c(0,17), breaks = c(5,10,15))+
   scale_y_continuous(limits = c(-4,4), labels = c(-4,-2,0,2,4))
-p3 <- ggMarginal(p, margins = 'y', color="red", size=4)
-p3
-ggsave('~/github/bmi_206/project/fig2d.pdf')
+## calculate percent of dec fc
+pct = paste0(round((nrow(fig2d %>% filter(log2(fc)<0))/nrow(fig2d))*100, 1), "%")
+## Plot density with pct text above
+p2 = ggplot(fig2d, aes(log2(fc)))+
+  # geom_histogram(color='red', bins=1000)+
+  geom_density(color='red')+
+  theme_pubr()+
+  coord_flip()+
+  geom_vline(xintercept=0, linetype="dashed", color = "black")+
+  scale_x_continuous(limits = c(-4,4), labels = c(-4,-2,0,2,4))+
+  theme(axis.title.y = element_blank())+
+  xlab('Density')+
+  annotate("text", x = -2, y = 0.3, label = pct, size = 5)
+g = grid.arrange(arrangeGrob(p, p2, ncol=2, widths=c(2,1)))
+g
+ggsave('~/github/bmi_206/project/fig2d_normed_gridArrange.png', g)
+# p3 <- ggMarginal(p, margins = 'y', color="red", size=4)
+# p3
+# ggsave('~/github/bmi_206/project/fig2d_normed.pdf')
+
 ## Fig 2E
-fig2e = infile %>% 
+fig2e = infile %>%
   mutate(fc = AMD1_Retina_MacL/AMD1_Retina_MacR,
          avg = rowMeans(select(., grep('AMD1_Retina_Mac', colnames(infile))), na.rm = TRUE))
 # avg = rowMeans(select(., grep('Retina', colnames(infile))), na.rm = TRUE))
-p2 = ggplot(fig2e, aes(log2(avg), log2(fc)))+
+p3 = ggplot(fig2e, aes(log2(avg), log2(fc)))+
   geom_point(color='grey')+
   geom_smooth(se = FALSE)+
   theme_pubr()+
@@ -54,6 +75,52 @@ p2 = ggplot(fig2e, aes(log2(avg), log2(fc)))+
   ylab('Log2(FC) of Left vs Right Eye in  AMD1')+
   scale_x_continuous(limits = c(0,17), breaks = c(5,10,15))+
   scale_y_continuous(limits = c(-4,4), labels = c(-4,-2,0,2,4))
-p4 <- ggMarginal(p2, margins = 'y', color="red", size=4)
-p4
-ggsave('~/github/bmi_206/project/fig2e.pdf')
+## calculate percent of dec fc
+pct = paste0(round((nrow(fig2e %>% filter(log2(fc)<0))/nrow(fig2e))*100, 1), "%")
+## Plot density with pct text above
+p4 = ggplot(fig2e, aes(log2(fc)))+
+  # geom_histogram(color='red', bins=1000)+
+  geom_density(color='red')+
+  theme_pubr()+
+  coord_flip()+
+  geom_vline(xintercept=0, linetype="dashed", color = "black")+
+  scale_x_continuous(limits = c(-4,4), labels = c(-4,-2,0,2,4))+
+  theme(axis.title.y = element_blank())+
+  xlab('Density')+
+  annotate("text", x = -2, y = 0.3, label = pct, size = 5)
+g2 = grid.arrange(arrangeGrob(p3, p4, ncol=2, widths=c(2,1)))
+g2
+ggsave('~/github/bmi_206/project/fig2e_normed_gridArrange.png', g2)
+# p5 <- ggMarginal(p3, margins = 'y', color="red", size=4)
+# p4
+# ggsave('~/github/bmi_206/project/fig2e_normed.pdf')
+
+## https://www.geeksforgeeks.org/kolmogorov-smirnov-test-in-r-programming/
+library(dgof)
+ks.test(fig2d$fc, fig2e$fc)
+ref2d = rnorm(nrow(fig2d), mean=0, sd = sd(fig2d$fc))
+t.test(ref2d, log2(fig2d$fc), paired = TRUE, alternative = "two.sided")
+wilcox.test(ref2d, log2(fig2d$fc), paired = TRUE, alternative = "two.sided")
+ks.test(ref2d, log2(fig2d$fc), alternative = "l")
+plot(ecdf(ref2d),  
+     xlim = range(c(ref2d, log2(fig2d$fc))),  
+     col = "blue") 
+plot(ecdf(log2(fig2d$fc)),  
+     add = TRUE,  
+     lty = "dashed", 
+     col = "red")
+ref2e = rnorm(nrow(fig2e), mean=0, sd = sd(fig2e$fc))
+t.test(ref2e, log2(fig2e$fc), paired = TRUE, alternative = "two.sided")
+wilcox.test(ref2e, log2(fig2e$fc), paired = TRUE, alternative = "two.sided")
+ks.test(ref2e, log2(fig2e$fc), alternative = "l")
+plot(ecdf(ref2e),  
+     xlim = range(c(ref2e, log2(fig2e$fc))),  
+     col = "blue") 
+plot(ecdf(log2(fig2e$fc)),  
+     add = TRUE,  
+     lty = "dashed", 
+     col = "red")
+
+wilcox.test(log2(fig2d$fc), log2(fig2e$fc), paired = TRUE, alternative = "two.sided")
+wilcox.test(ref2e, log2(fig2e$fc), paired = TRUE, alternative = "two.sided")
+wilcox.test(ref2d, log2(fig2d$fc), paired = TRUE, alternative = "two.sided")
